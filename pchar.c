29c488b40f88fdd35edfedb3f8e47d1d27314dfa
Abderrahim LAHYANI <lahyani.ad@gmail.com>
2023-09-21 21:11:33 +0100

    Change range to 0-127

--- pchar.c
+++ pchar.c
@@ -15,7 +15,7 @@ void pchar(stack_t **h, unsigned int line_number)
 		exit(EXIT_FAILURE);
 	}
 
-	if ((*h)->n < 0 || (*h)->n > 255)
+	if ((*h)->n < 0 || (*h)->n > 127)
 	{
 		fprintf(stderr, "L%u: can't pchar, value out of range\n",
 			line_number);
